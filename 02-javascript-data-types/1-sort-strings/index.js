9eb0d13c7e6de225c70c803fdbc043287084e605
Yuliya Ivanskaya <yulia_kolo@mail.ru>
2022-10-29 23:26:32 +0900

    added solution to les02 task 1-sort

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,9 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    let k = 1;
+    if (param === 'desc') { k = -1; }
+    const sortedArr = arr.slice();
+    sortedArr.sort( (str1, str2) => str1.localeCompare(str2, ["ru", "en"], { caseFirst: "upper" }) * k );
+    return sortedArr;
 }
