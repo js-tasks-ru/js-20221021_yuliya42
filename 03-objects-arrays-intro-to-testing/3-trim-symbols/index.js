718c8fd1dde9e21c24c00d739a8aa0e507aff775
Yuliya Ivanskaya <yulia_kolo@mail.ru>
2022-11-04 22:35:31 +0900

    les 3 task 3-trim-symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,18 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    
+    function countChar (char, pos) {        
+        if (resultStr[pos] !== char || pos < 0) return 0;
+        let count = 1 + countChar(char, pos-1);
+        return count;        
+    }
 
+    let resultStr = '';
+    for ( let i = 0; i < string.length; i++ ) {
+        if ( countChar(string[i], resultStr.length-1) === size) continue;
+        resultStr += string[i];
+    }
+
+    return resultStr;
 }
