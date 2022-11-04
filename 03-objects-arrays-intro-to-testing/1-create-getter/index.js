c17019ff19d43f2eba858007da25bc1cef28112f
Yuliya Ivanskaya <yulia_kolo@mail.ru>
2022-11-04 20:40:31 +0900

    les 3 task 1-createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,14 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    return function (obj) { 
+        let result = obj;
+        const attrs = path.split('.'); 
+         
+        attrs.forEach( item => 
+                        { if (result === undefined ) {  return; }
+                          result = result[item];
+                        });
+        return result; 
+    };
 }
