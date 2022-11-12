7c7ce59d9e7aabc1cda77204bdd4f63cfaa8252a
Yuliya Ivanskaya <yulia_kolo@mail.ru>
2022-11-12 21:49:08 +0900

    fixed notes of les 04, solved les 5 NotificationMessage

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,5 +1,5 @@
 export default class ColumnChart {
-    constructor( { data = [], label = "", value = 0, link = null, formatHeading = data => data} = {} ) { 
+    constructor( { data = [], label = "", value = 0, link = "", formatHeading = data => data} = {} ) { 
         // подскажите, пожалуйста
         // не понимаю, почему значения по умолчанию не отрабатывают? Потому что передаются в объекте? Здесь пришлось дополнительно проверять.
         this.data = data;
@@ -13,24 +13,24 @@ export default class ColumnChart {
     
     getTemplate() {
 
-        const str_link =  this.link !== null ? `<a href="${this.link}" class="column-chart__link">View all</a>` : ``;
+        const strLink =  this.link === `` ? this.link : `<a href="${this.link}" class="column-chart__link">View all</a>`;
 
-        const str_heading = this.formatHeading(this.value);
+        const strHeading = this.formatHeading(this.value);
         
-        const str_columns = this.getHTMLcolumns(this.data);
+        const strColumns = this.getHTMLcolumns(this.data);
         
         return `
             <div class="column-chart column-chart_loading"  style="--chart-height: ${this.chartHeight}">
                 <div class="column-chart__title">
                     ${this.label} 
-                    ${str_link}
+                    ${strLink}
                 </div>
                 <div class="column-chart__container">
                     <div data-element="header" class="column-chart__header">
-                        ${str_heading}
+                        ${strHeading}
                     </div>
                     <div data-element="body" class="column-chart__chart">
-                        ${str_columns}
+                        ${strColumns}
                     </div>
                 </div>
             </div>`;
@@ -56,12 +56,12 @@ export default class ColumnChart {
     }
 
     getHTMLcolumns ( dataArr ) {        
-        let str_columns = "";         
+        let strColumns = "";         
         const columnProps = this.getColumnProps(dataArr); 
         columnProps.forEach( (item) => 
-            str_columns += `<div style="--value: ${item.value}" data-tooltip="${item.percent}"></div>`);
+            strColumns += `<div style="--value: ${item.value}" data-tooltip="${item.percent}"></div>`);
 
-        return str_columns;
+        return strColumns;
     }
 
     update ( updatedArr = [] ) {  
@@ -76,7 +76,7 @@ export default class ColumnChart {
 
     getColumnProps(data) {
         const maxValue = Math.max(...data);
-        const scale = 50 / maxValue;
+        const scale = this.chartHeight / maxValue;
       
         return data.map(item => {
           return {
--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,49 @@
 export default class NotificationMessage {
+    static elementInDOM;
 
+    constructor ( message = '', { duration = 1000, type = 'succses'} = {} ) {
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+        
+        this.createElement();
+    }
+
+    getTemplate() {
+        return `
+          <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">            
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+            </div>
+          </div>
+        `;
+    }
+    
+    show(parent = document.body) {
+        if ( NotificationMessage.elementInDOM ) { this.remove(); }   
+           
+        setTimeout(() => this.remove(), this.duration);
+        
+        parent.append(this.element);
+        NotificationMessage.elementInDOM = this.element;  
+    }
+
+    createElement () {       
+        const element = document.createElement("div");     
+        element.innerHTML = this.getTemplate();
+        this.element = element.firstElementChild;
+    } 
+
+    remove() {
+        this.element.remove();
+        if (NotificationMessage.elementInDOM) { NotificationMessage.elementInDOM.remove();}
+    }
+
+    destroy() {
+        this.remove();
+    }
 }
